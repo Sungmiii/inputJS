8720622539cf69f4425c96f0f2257fd65384852f
Sungmiii <christyksmile@gmail.com>
2019-06-07 16:44:14 +1200

    refactoring function(no repeat)

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ let button = document.querySelector('.button').textContent = 'submit'
 
 function placehoder(input) {
     let name = input.getAttribute('name')
+
+    test(input, name)
+
+    input.addEventListener('focus', () => {
+        input.setAttribute('placeholder', '')
+    })
+
+    input.addEventListener('blur', () => test(input, name))
+}
+
+function test(input, name) {
     input.setAttribute('placeholder', function (name) {
         if (name === 'firstName') {
             return 'First Name'
@@ -11,18 +22,6 @@ function placehoder(input) {
             return 'Last Name'
         }
     }(name))
-
-    input.addEventListener('focus', () => {
-        input.setAttribute('placeholder', '')
-    })
-
-    input.addEventListener('blur', () => input.setAttribute('placeholder', function (name) {
-        if (name === 'firstName') {
-            return 'First Name'
-        } else if (name === 'lastName') {
-            return 'Last Name'
-        }
-    }(name)))
 }
 
 placehoder(input[0])
